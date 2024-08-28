0fce35f562c820cfdc30f55cf0b945a110ec9cc0
Vishesh-Nagar <nagarvishesh16@gmail.com>
2024-08-28 22:50:46 +0530

    Final Commit

---
+++ include/hospital.hh
@@ -0,0 +1,49 @@
+#ifndef HOSPITAL
+#define HOSPITAL
+using namespace std;
+#include <map>
+#include <string>
+#include <vector>
+
+
+#include "./ambulance.hh"
+#include "./appointment.hh"
+#include "./doctor.hh"
+#include "./driver.hh"
+#include "./nurse.hh"
+#include "./patient.hh"
+
+
+class hospital {
+private:
+  // map<id, object>
+  static map<int, doctor> doctorsList;
+  static map<int, patient> patientsList;
+  static map<int, nurse> nursesList;
+  static map<int, driver> driversList;
+  static map<int, ambulance> ambulancesList;
+  static map<int, appointment> appointmentsList;
+
+  static const int doctorsLimit;
+  static const int nursesLimit;
+  static const int driversLimit;
+  static const int ambulancesLimit;
+  static const int appointmentsLimit;
+
+  friend class doctor;
+  friend class patient;
+  friend class nurse;
+  friend class driver;
+  friend class ambulance;
+  friend class appointment;
+
+public:
+  static void printDoctors();
+  static void printPatients();
+  static void printNurses();
+  static void printDrivers();
+  static void printAmbulances();
+  static void printAppointments();
+};
+
+#endif // !HOSPITAL
